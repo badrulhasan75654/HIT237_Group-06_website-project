deb8e27f6b5496ace42ce0d71806c9f579bff6cf
badrulhasan75654 <165794376+badrulhasan75654@users.noreply.github.com>
2024-04-06 00:17:35 +0930

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const cardsData = [
+  { title: 'Card 1: My Details', content: 'Name: Md Badrul Hasan Bhuiyan                  Student ID: s361204.' },
+  { title: 'Card 2', content: 'This is the content of Card 2.' },
+  { title: 'Card 3', content: 'This is the content of Card 3.' }
+];
+
+const cardContainer = document.getElementById('card-container');
+
+cardsData.forEach(data => {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const title = document.createElement('h2');
+  title.textContent = data.title;
+
+  const content = document.createElement('p');
+  content.textContent = data.content;
+
+  card.appendChild(title);
+  card.appendChild(content);
+
+  cardContainer.appendChild(card);
+});
